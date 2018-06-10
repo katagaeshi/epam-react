b32835e80798f6bae4f7ba004494063fc66fdcdc
Yauheni Kireyeu <Yauheni_Kireyeu@epam.com>
2018-06-10 22:13:12 +0300

    e2e test updated

--- test/e2e/RunSearchRequest.js
+++ test/e2e/RunSearchRequest.js
@@ -9,17 +9,23 @@ module.exports = {
 
   after(browser, done) {
     chromedriver.stop();
-
-    done();
+    browser.end(function() {
+      done();
+    });
   },
 
   'Click search button': function (browser) {
     browser
       .url(browser.launchUrl)
       .resizeWindow(1280, 800)
-      .waitForElementVisible('.SearchButton', 1000)
-      .click('.SearchButton')
-      .end();
+      .waitForElementVisible('#root > div > div > div.SearchPanel-searchPanel-0-1-2 > div.SearchPanel-searchButtonContainer-0-1-4 > button', 1000)
+      .setValue('#root > div > div > div.SearchPanel-searchPanel-0-1-2 > input', 'Jack')
+      .click('#root > div > div > div.SearchPanel-searchPanel-0-1-2 > div.SearchPanel-searchButtonContainer-0-1-4 > button')
+      .pause(1000)
+      .getText('#root > div > div > div:nth-child(2) > span', function(text) {
+        this.assert.equal(text.value, '13 movies found');
+        browser.end();
+      });
   },
 };
 
